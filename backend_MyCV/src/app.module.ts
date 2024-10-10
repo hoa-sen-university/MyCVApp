71e78a7e1a203cb10f930910cbff2c9fbd9350db
Truong Giang <giang.nt09845@sinhvien.hoasen.edu.vn>
2024-10-10 15:22:08 +0700

    backend

--- backend_MyCV/src/app.module.ts
+++ backend_MyCV/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './userService/user.controller';
+import { UserService } from './userService/user.service';
+import { User, UserSchema } from './schemas/user.schema';
+
+const MONGODB_URI = 'mongodb+srv://giang:123@admin.9qcla.mongodb.net/demo?retryWrites=true&w=majority&appName=Admin';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(MONGODB_URI),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
---
+++ backend_MyCV/src/userService/user.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
+import { User } from '../schemas/user.schema';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<User> {
+    return this.userService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto): Promise<User> {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<User> {
+    return this.userService.remove(id);
+  }
+}
